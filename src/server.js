dfcb789365e3eebe74ed228afa85c35beec91b3f
velychkovych <boda.velich@gmail.com>
2022-11-22 01:48:13 +0200

    add cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('./config');
 const jobs = require('./jobs');
 
 const transactionController = require('./controllers/transaction-controller');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/api/v1/transactions', transactionController);
 

80ed821a50756258b2a1ba6354ddd9b5124eb2b3
velychkovych <boda.velich@gmail.com>
2022-11-22 00:59:28 +0200

    fix

--- src/data-access/transaction-dao.js
+++ src/data-access/transaction-dao.js
@@ -14,6 +14,7 @@ module.exports = (Transaction) => {
 
 	async function findOne() {
 		const transaction = await Transaction.findOne({});
+		if (!transaction) return null;
 		return formatTransaction(transaction);
 	}
 
